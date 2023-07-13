9882896f5c04536b0368c22d5a6f67d840843b48
BnKeTia <bernardnketia51@gmail.com>
2023-07-13 21:18:02 +0000

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0
+ */
 int main(void)
 {
-    char message[] = "Programming is like building a multilingual puzzle\n";
-    puts(message);
-    return 0;
+puts("\"Programming is like building a multilingual puzzle
+");
+return (0);
 }
-
