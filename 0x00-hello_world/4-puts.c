02ed559788691b52af32dd12a1abb99b2f3be401
BnKeTia <bernardnketia51@gmail.com>
2023-07-13 20:52:46 +0000

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h
+  /**
+   * main - a program that prints a line with puts function without using printf
+   * Return: 0 (complete)
+   */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
