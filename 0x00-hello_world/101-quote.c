9cd2b74b916c742dbf65b2b12870a07f90af172a
BnKeTia <bernardnketia51@gmail.com>
2023-07-14 00:41:36 +0000

    101 quote last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints exact words and new line to the standard error
- * Return 1 (completed)
+ * main - Entry point
+ *
+ * Return: 1 (complete)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1)
+write(2,
+"and that piece of art is useful\" -DoraKorpar, 2015-10-19\n", 59);
+return (1);
 }
