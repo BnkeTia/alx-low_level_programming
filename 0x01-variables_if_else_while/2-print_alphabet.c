e6e98b86f39820713e77e7f8ecc25220259f742f
BnKeTia <bernardnketia51@gmail.com>
2023-07-14 23:51:11 +0000

    2-Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ * Return: 0 (complete)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
