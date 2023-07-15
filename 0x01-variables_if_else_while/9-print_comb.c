08d92c156c06b61ea53b2d96495d434811cc0734
BnKeTia <bernardnketia51@gmail.com>
2023-07-15 01:48:41 +0000

    Print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,16 +6,17 @@
 
 int main(void)
 {
-	int n;
+	int m;
 
-	for (n = 0; n < 10; n++)
+	for (m = 0; m < 10; m++)
 	{
-		putchar(n + '0');
-	}
+		putchar(m + '0');
 
-	for (n = 97; n <= 102; n++)
-	{
-		putchar(n);
+		if (m < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
