1d14ff0ed23a536bc52b684186521c9d50a92366
BnKeTia <bernardnketia51@gmail.com>
2023-07-15 01:39:08 +0000

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 /**
  * main - Point of entry
- * Return: 0 (completed)
+ * Return: 0 (complete)
  */
 
 int main(void)
 {
 	int n;
 
-	for (n = 122; n >= 97; n--)
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+
+	for (n = 97; n <= 102; n++)
 	{
 		putchar(n);
 	}
+
 	putchar('\n');
 
 	return (0);
