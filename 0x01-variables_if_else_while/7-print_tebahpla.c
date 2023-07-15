2c20d62d1f90c41329ab24eb4858ef913794970a
BnKeTia <bernardnketia51@gmail.com>
2023-07-15 01:22:04 +0000

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ * Return: 0 (completed)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 122; n >= 97; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
