7d323632b72a61a0803e569011fefe09dec585f3
BnKeTia <bernardnketia51@gmail.com>
2023-07-15 00:03:04 +0000

    3-print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Point of Entry
+ * Return: 0 (complete)\
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+
+		putchar(x);
+
+	for (x = 65; x <= 90; x++)
+
+		putchar(x);
+
+	putchar('\n');
+
+	return (0);
+}
