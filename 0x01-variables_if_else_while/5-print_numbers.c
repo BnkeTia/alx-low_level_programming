09f65f3b5c25d03289a409a4a308d8026d9dbd4e
BnKeTia <bernardnketia51@gmail.com>
2023-07-15 01:03:08 +0000

    5-print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Point of entry
+ * Return: 0 (complete)
+ */
+
+int main(void)
+{
+
+	int n;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+
+	printf("\n");
+	return (0);
+}
