f3347b47b5e5f1af8b9c5899b346f82571ee4e66
BnKeTia <bernardnketia51@gmail.com>
2023-07-18 20:02:11 +0000

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabets will be printed in lowercase.
+ * Return: 0 (complete)
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+
+}
