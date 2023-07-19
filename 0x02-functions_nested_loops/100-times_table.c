a9b4f28a8bfdcfd3e0507474458df4325004d749
BnKeTia <bernardnketia51@gmail.com>
2023-07-19 15:37:43 +0000

    Task 12 multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,7 +6,7 @@
  */
 void print_times_table(int n)
 {
-	int x, y, xy;
+	int x;
 
 	if (n < 0 || n > 15)
 	{
@@ -15,46 +15,52 @@ void print_times_table(int n)
 
 	for (x = 0; x <= n; x++)
 	{
-		for (y = 0; y <= n; y++)
-		{
-			xy = x * y;
-
-			if (y == 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar(' ');
-			}
-
-			if (xy < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (xy < 100)
-			{
-				_putchar(' ');
-			}
-			if (xy >= 100)
-			{
-				_putchar((xy / 100) + '0');
-			}
-			if (xy >= 10)
-			{
-				_putchar(((xy / 10) % 10) + '0');
-			}
-
-			_putchar((xy % 10) + '0');
-
-			if (y < n)
-			{
-				_putchar(',');
-			}
-
-		}
-
+		print_times_row(x, n);
 		_putchar('\n');
 	}
+}
+/**
+ * print_times_row - Prints a row of the n times tables.
+ * @x: The row number to print.
+ * @n: The number of columns (up to x) to print for the row
+ */
+void print_times_row(int x, int n)
+{
+	int y, xy;
+
+	for (y = 0; y <= n; y++)
+	{
+		xy = x * y;
+		if (y == 0)
+		{
+			_putchar('0');
+		}
+		else
+		{
+			_putchar(' ');
+		}
+		if (xy < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		else if (xy < 100)
+		{
+			_putchar(' ');
+		}
+		if (xy >= 100)
+		{
+			_putchar((xy / 100) + '0');
+		}
+		if (xy >= 10)
+		{
+			_putchar(((xy / 10) % 10) + '0');
+		}
+		_putchar((xy % 10) + '0');
+		if (y < n)
+		{
+			_putchar(',');
+		}
+	}
+		_putchar('\n');
 }
