d1fa31701d3899ccfd62d277496d5e84d094929c
BnKeTia <bernardnketia51@gmail.com>
2023-07-19 10:26:25 +0000

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *times_table - Prints the 9 times table.
+ *
+ *Return: void
+ */
+void times_table(void)
+{
+	int x, y, xy;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			xy = x * y;
+			if (y != 0)
+			{
+				_putchar(',');
+				if (xy < 10)
+					_putchar(' ');
+			}
+			if (xy >= 10)
+			{
+				_putchar(xy / 10 + '0');
+				_putchar(xy % 10 + '0');
+			}
+			else
+			{
+				_putchar(xy + '0');
+			}
+		}
+		_putchar(xy + '0');
+	}
+}
