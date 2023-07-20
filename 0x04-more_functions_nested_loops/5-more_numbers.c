bf0444b1d1ab47ca6429e5ea6b28406639187186
BnKeTia <bernardnketia51@gmail.com>
2023-07-20 21:49:49 +0000

    5-more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,20 +4,23 @@
  * more_numbers - prints 10times the numbers from 0 to 14
  * Return: numbers from 0 to 14
  */
-void more_numbers(void);
+void more_numbers(void)
 {
-	int i, j;
+	int x, y;
 
-	for (i = 0; i < 10; i++)
+	for (x = 0; x < 10; x++)
 	{
-	for (j = 0; j <= 14; j++)
+
+	for (y = 0; y <= 14; y++)
 	{
-	if (j > 9)
+	if (y > 9)
 	{
-	_putchar((j / 10) + '0');
+
+	_putchar((y / 10) + '0');
 	}
-	_putchar((j % 10) + '0');
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
+
 	}
 }
