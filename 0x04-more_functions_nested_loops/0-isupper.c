9f846dfac35f32db501f0cb49314e25b33cc492c
BnKeTia <bernardnketia51@gmail.com>
2023-07-20 19:38:05 +0000

    task 0 Isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a letter is upper
+ * @x: The number to be checked
+ *
+ * Return: 1 for the uppercase and 0 for otherwise
+ */
+int _isupper(int x)
+{
+	if (x >= 'A' && x <= 'Z')
+	{
+	return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Project on functions and nested loops. Projects have been dropping like manner to the israelites, on a daily basis.
