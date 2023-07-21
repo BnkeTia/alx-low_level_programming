97749f8ec6e863da22a9bf890f04d2eac92e9210
BnKeTia <bernardnketia51@gmail.com>
2023-07-21 00:04:57 +0000

    Triangle printing

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 
 	if (size <= 0)
@@ -19,12 +19,18 @@ void print_triangle(int size)
 	{
 	for (i = 1; i <= size; i++)
 	{
+	for (k = size - i; k >= 1; k--)
+	{
+	_putchar(' ');
+	{
 	for (j = 1; j <= i; j++)
 	{
 	_putchar('#');
 
 	}
 	_putchar('\n');
+	}
+	}
 	}
 	}
 }
