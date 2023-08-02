7567699d50485dd3ad16e1f049c4a05252762dcb
BnkeTia <bernardnketia51@gmail.com>
2023-08-02 16:12:46 -0700

    5-sqrt recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * sqrt2 - A function that fascilitate movement from 1 to n
+ * @a: same number as n
+ * @b: number that moves from 1 to n
+ * Return: 1 (completed)
+ */
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+
+	else if (b * b > a)
+	{
+		return (-1);
+	}
+
+	return (sqrt2(a, b + 1));
+}
+/**
+ * _sqrt_recursion - A function that returns the natural square root of n
+ * @n: Integer
+ * Return: On sucicess 1.
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (sqrt2(n, 1));
+}
