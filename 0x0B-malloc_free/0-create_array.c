9489339953a6166eb7cc470e5b467ee2a72e1977
BnKeTia <bernardnketia51@gmail.com>
2023-08-08 13:44:47 +0000

    0-create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - A function that creates an array of chars.
+ * @size: size of array
+ * @c: specific char
+ *Return: A pointer to the array or NULL if it fails.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *a;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	a = (char *)malloc(size * sizeof(char));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		a[i] = c;
+	}
+
+	return (a);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This a directory of the project on memory allocation and free. kit consists of six tasks in total.
