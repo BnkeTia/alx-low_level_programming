e851ff9b88f2cd8a69f4e301c21855d1e264e309
BnKeTia <bernardnketia51@gmail.com>
2023-08-29 11:07:30 +0000

    code fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,15 @@ size_t print_listint(const listint_t *h)
 {
 	size_t numN = 0;
 
+	if (h == NULL)
+		return (0);
+
 	while (h != NULL)
 	{
 		numN++;
-		printf("%d\n", h->node_value);
+		printf("%i\n", h->node_value);
 		h = h->next;
+		return (numN);
 
 	}
 
