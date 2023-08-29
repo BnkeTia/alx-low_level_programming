ceea56b01c76806e01ce247f0a879a069e7b5711
BnKeTia <bernardnketia51@gmail.com>
2023-08-29 03:08:56 +0000

    pleasing madam betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,8 +5,13 @@
 #include <stddef.h>
 #include <stdio.h>
 
-/* structure definition for linked list */
-typedef struct listint_s {
+/**
+ * struct listint_s - defines the structure of the linked list.
+ * @node_value: integer field that hold the value of a node in the list.
+ * @next: pointer to the next element in the linked list.
+ */
+typedef struct listint_s
+{
 	int node_value;
 
 	struct listint_s *next;
