d451b9685fdcb0c05256198050e83e9be22899b9
BnKeTia <bernardnketia51@gmail.com>
2023-08-30 23:25:30 +0000

    task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint_safe - A function that frees a linked list
+ * @h: pointer to the pointer of the head of the list.
+ * Return: The size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	listint_t *nC, *tmp;
+
+	if (h == NULL)
+		return (0);
+
+	nC = *h;
+	while (nC != NULL)
+	{
+		size++;
+		tmp = nC;
+		nC = nC->next;
+		free(tmp);
+
+		if (nC == *h)
+		{
+			*h = NULL;
+			break;
+		}
+	}
+	return (size);
+}
