3920bf54b2f382889921caf511decde9d2d6919b
BnKeTia <bernardnketia51@gmail.com>
2023-10-14 21:42:02 +0000

    100 password

--- 0x17-doubly_linked_lists/crackme4.py
+++ 0x17-doubly_linked_lists/crackme4.py
@@ -6,14 +6,13 @@
 # Embedded file name: 100-crackme.py
 # Compiled at: 2017-01-06 00:44:37
 # Size of source mod 2**32: 195 bytes
-pwd: str = input(__prompt='Password: ')
+pwd = input('Password: ')
 ok = 'Zen of Python'
 ok = ok + ' C'
-ok: str = ok[1:3] + ok[3] + ok[14] + ok[3] + ok[7:9] + ok[:6][::-1]
+ok = ok[1:3] + ok[3] + ok[14] + ok[3] + ok[7:9] + ok[:6][::-1]
 if pwd == ok:
     print('OK')
 else:
     print('KO')
-# okay decompiling crackme4.pyc
-with open(file='100-password', mode='w') as f:
-    f.write(__s/ok)
+
+print(ok)
