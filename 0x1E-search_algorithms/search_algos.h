96710320a0810b5afea41202e6c27e34d8f35f93
BnKeTia <bernardnketia51@gmail.com>
2024-02-06 22:11:36 +0000

    added standard libraries

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef BISKIT_SEARCH_ALGOS_H
 #define BISKIT_SEARCH_ALGOS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /* Function prototypes of mandatory tasks */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
