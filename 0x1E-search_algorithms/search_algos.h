d69f084e7cd5313d675f4cc26fc9dab31c8ca179
BnKeTia <bernardnketia51@gmail.com>
2024-02-06 22:10:01 +0000

    prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,9 @@
 #ifndef BISKIT_SEARCH_ALGOS_H
 #define BISKIT_SEARCH_ALGOS_H
 
+/* Function prototypes of mandatory tasks */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 
 #endif /* BISKIT_SEARCH_ALGOS_H */
